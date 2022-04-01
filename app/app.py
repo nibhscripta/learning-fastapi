a094067a762aac8c2ab672ac26750022aedeea85
tux <nibhscripta@tutanota.com>
2022-04-01 11:50:12 -0500

    createm response schemas

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
-from . import models
+from . import models, schemas
 from .database import engine, get_db
-from .schemas import Post
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -12,35 +12,32 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-
-
-
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"posts": posts}
+    return posts
 
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {'data': new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} was not found')
-    return {'data': post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -53,12 +50,12 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id:int, updated_post:Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.PostResponse)
+def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} does not exist.')
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {'detail': post_query.first()}
+    return post_query.first()
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-class Post(BaseModel):
+
+class PostBase(BaseModel):
     title: str
     content: str 
     published: bool = True
+
+    
+class PostCreate(PostBase):
+    pass
+
+
+#response sent when a post is retrieved
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+        
+    class Config:
+        orm_mode = True
