e57e2be7a4bd25a5c484104a7b159855cbed2dd2
tux <nibhscripta@tutanota.com>
2022-04-01 15:06:06 -0500

    hashing passwords

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from passlib.context import CryptContext
 
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
@@ -63,7 +61,7 @@ def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(g
 
 @app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.CreateUserResponse)
 def create_user(user: schemas.UserCreate ,db: Session = Depends(get_db)):
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
     db.add(new_user)
