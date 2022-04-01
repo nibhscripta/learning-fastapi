57cf4a33058ac70b6b182e1e8f88c151c267669a
nibhscripta <95943200+nibhscripta@users.noreply.github.com>
2022-04-01 11:15:19 -0500

    Add comment about database credentials

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#not real credentials
 SQLALCHEMY_DATABASE_URL = 'postgresql://dev:dev@localhost/dev'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
