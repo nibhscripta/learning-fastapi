d2cd96897fbb22d01326ecc9bf7a1dd3b0c731aa
tux <nibhscripta@tutanota.com>
2022-04-02 14:17:19 -0500

    add voting system

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 
-from .config import settings
 from . import models
 from .database import engine
-from .routers import post, user, auth
-from app import database
+from .routers import post, user, auth, vote
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -14,6 +12,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
 @app.get("/")
---
+++ app/routers/vote.py
@@ -0,0 +1,32 @@
+from multiprocessing import synchronize
+from fastapi import Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing import List, Optional
+
+from .. import models, schemas, utils, oauth2
+from ..database import get_db
+
+
+router = APIRouter(
+    prefix='/vote',
+    tags=['Votes']  
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
+    queried_vote = vote_query.first()
+    if vote.dir == 1:
+        if queried_vote:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "voted"}
+    else:
+        if not queried_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
+        queried_vote.delete(synchronize_session=False)
+        db.commit()
+        return {'message': 'Vote deleted'}
